12a1d3d564e1831433ee16d204751e3aaf55665e
ziggy <ziggyamgar@gmail.com>
2024-06-11 22:32:47 +0200

    1

--- ft_printf.c
+++ ft_printf.c
@@ -1,4 +1,26 @@
-int	ft_printf(const char *, ...)
-{
+#include <unistd.h>
+#include <stdarg.h>
 
+int	ft_printf(const char *str, ...)
+{
+	int	i;
+	va_list ap;
+
+	i = 0;
+	va_start(ap, str);
+	while (str[i] ! = '\0')
+	{
+		if (str[i]
+
+		else
+			write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main()
+{
+	char	*str = "Ziggy";
+	ft_printf("Bonjour je m'appelle %d", str);
+	return (0);
 }
