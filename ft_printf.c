3ff0e2cda48b2582407639da62877e3d8df1ad17
Ziggy Amgar <zamgar@paul-f4Ar7s1.clusters.42paris.fr>
2024-06-13 18:12:52 +0200

    5

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: zamgar <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 12:48:28 by zamgar            #+#    #+#             */
-/*   Updated: 2024/06/12 18:33:04 by zamgar           ###   ########.fr       */
+/*   Updated: 2024/06/13 17:37:42 by zamgar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,16 @@ void	ft_putstr_fd(char *str, int fd)
 	int	i;
 
 	i = 0;
+	if (str == NULL)
+	{
+		ft_putchar_fd('(', 1);
+		ft_putchar_fd('n', 1);
+		ft_putchar_fd('u', 1);
+		ft_putchar_fd('l', 1);
+		ft_putchar_fd('l', 1);
+		ft_putchar_fd(')', 1);
+		return ;
+	}
 	while (str[i] != '\0')
 	{
 		ft_putchar_fd(str[i], fd);
@@ -63,16 +73,6 @@ void	ft_unsigned_putnbr_fd(unsigned int n, int fd)
 		ft_putchar_fd('0', fd);
 		return ;
 	}
-	/*if (n == -2147483648)
-	{
-		ft_putstr_fd("-2147483648", fd);
-		return ;
-	}*/
-	/*if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}*/
 	if (n >= 10)
 	{
 		ft_putnbr_fd(n / 10, fd);
@@ -82,35 +82,81 @@ void	ft_unsigned_putnbr_fd(unsigned int n, int fd)
 		ft_putchar_fd(n + '0', fd);
 }
 
-//n % 16 - 10 + 'a';
-
 void	ft_dec_into_hex(int n, int b)
 {
-	int	div;
-	int	mod;
+	int	temp;
 	char	*string;
+	char	*array;
+	int	i;
 
+	i = 0;
+	temp = 0;
+	string = (char *)malloc(sizeof(char) * 17);
+	array = (char *)malloc(sizeof(char) * 100);
+	if (string == NULL || array == NULL)
+		return ;
+	string = "0123456789abcdef";
+	temp = 100;
+	while(i < 100)
+	{
+		array[i] = '\0';
+		i++;
+	}
+	i = 0;
 	if (b == 1)
 		string = "0123456789ABCDEF";
 	if (b == 0)
 		string = "0123456789abcdef";
-	div = n / 16;
-	mod = n % 16;
-
-	ft_putchar_fd(string[div], 1);
-	ft_putchar_fd(string[mod], 1);
+	while (n > 0)
+	{
+		temp = n % 16;
+		array[i] = string[temp];
+		i++;
+		n = n / 16;
+	}
+	ft_putstr_fd(array, 1);
+	free(string);
+	free(array);
 }
 
-/*void	ft_putptr(void *adress)
+void	ft_long_dec_into_hex(unsigned long long n)
 {
+	unsigned long long	temp;
+	char	*string;
+	char	*array;
 	int	i;
-	char	*str;
 
 	i = 0;
+	temp = 0;
+	string = (char *)malloc(sizeof(char) * 17);
+	array = (char *)malloc(sizeof(char) * 100);
+	if (string == NULL || array == NULL)
+		return ;
+	string = "0123456789abcdef";
+	temp = 100;
+	while(i < 100)
+	{
+		array[i] = '\0';
+		i++;
+	}
+	i = 0;
+	while (n > 0)
+	{
+		temp = n % 16;
+		array[i] = string[temp];
+		i++;
+		n = n / 16;
+	}
+	ft_putstr_fd(array, 1);
+	free(string);
+	free(array);
+}
+
+void	ft_putptr(unsigned long long adress)
+{
 	ft_putstr_fd("0x", 1);
-	ft_dec_into_hex(&adress, 0);
-	//printf("%p", adress);
-}*/
+	ft_long_dec_into_hex(adress);
+}
 
 void	get_next_arg(va_list *ap, char c)
 {
@@ -118,8 +164,8 @@ void	get_next_arg(va_list *ap, char c)
 		ft_putchar_fd((va_arg(*ap, int)), 1);
 	else if (c == 's')
 		ft_putstr_fd(va_arg(*ap, char *), 1);
-//	else if (c == 'p')
-//		ft_putptr(va_arg(*ap, void*));
+	else if (c == 'p')
+		ft_putptr(va_arg(*ap, unsigned long long));
 	else if (c == 'd')
 		ft_putnbr_fd(va_arg(*ap, int), 1);
 	else if (c == 'i')
@@ -152,17 +198,17 @@ int	ft_printf(const char *str, ...)
 			ft_putchar_fd(str[i], 1);
 		i++;
 	}
-	return (i - 1);
+	return (i - 1); // probleme ne return pas la bonne taille
 }
 
 /*int	main()
 {
-	int	a = 45;
-	int	b = 45;
-	int	*x = &a;
-	int	*y = &b;
-
-	printf("x : %p\ny : %p\n\n", (void *)x, (void *)y);
-	ft_printf("ceci est l adresse de x : %p\n", (void *)x);
+	//printf("vrai printf :\n");
+	//printf(" NULL %s NULL ", (char *)NULL);
+	//printf("\n");
+	printf("%d", printf("%s%s%s", "test", "test", "test"));
+	//printf("my printf :\n");
+	//ft_printf("%s", "");
+	printf("\n");
 	return (0);
 }*/
